afc9168f60b65f339a1db465023e27453b366757
CJTozer <christopherjtozer@gmail.com>
2013-01-27 17:44:37 +0000

    More getter diags

--- Getter/GoogleGetter.py
+++ Getter/GoogleGetter.py
@@ -7,8 +7,11 @@ class GoogleGetter:
         self.base_url = 'http://finance.google.com/finance/info?q='
     
     # @@@ add some error handling...
-    def get_stock(self, symbol): 
-        lines = requests.get(self.base_url + symbol).text.splitlines()
+    def get_stock(self, symbol):
+        url = self.base_url + symbol
+        print url
+        lines = requests.get(url).text.splitlines()
+        print lines
         json_data = json.loads(''.join([x for x in lines if x not in ('// [', ']')]))
         return float(json_data["l"].replace(",", ""))
         
