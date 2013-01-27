dbadd5de3ec2a1a802b2fe80a87152b579e3ab3a
CJTozer <christopherjtozer@gmail.com>
2013-01-27 18:00:08 +0000

    Ok, so dreamhost won't let me make HTTP requests it seems...

--- Getter/GoogleGetter.py
+++ Getter/GoogleGetter.py
@@ -4,18 +4,13 @@ import json
 class GoogleGetter:
     
     def __init__(self):
-        #self.base_url = 'http://finance.google.com/finance/info?q='
-        self.base_url = 'http://httpbin.org/get?q=ARM'
+        self.base_url = 'http://finance.google.com/finance/info?q='
     
     # @@@ add some error handling...
     def get_stock(self, symbol):
-        url = self.base_url + symbol
-        print url
-        r = requests.get(url)
-        print r
-        print r.headers
+        r = requests.get(self.base_url + symbol)
+        r.raise_for_status()
         lines = r.text.splitlines()
-        print lines
         json_data = json.loads(''.join([x for x in lines if x not in ('// [', ']')]))
         return float(json_data["l"].replace(",", ""))
         
