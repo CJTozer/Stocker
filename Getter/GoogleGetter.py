c526dcc07e470575ef2396d7ab83afbca038bd14
Chris Tozer <christopherjtozer@gmail.com>
2013-02-05 22:42:07 +0000

    More resilience in the getter

--- Getter/GoogleGetter.py
+++ Getter/GoogleGetter.py
@@ -8,7 +8,13 @@ class GoogleGetter:
     
     def get_stock_value(self, symbol):
         self.logger.info("Retrieving stock %s from Google" % symbol)
-        r = requests.get(self.base_url, params={'q': "LON:%s" % symbol})
+        # Give it 3 goes...
+        for ii in range(3):
+            r = requests.get(self.base_url, params={'q': "LON:%s" % symbol})
+            self.logger.info("Response: %d" % r.status_code)
+            if r.status_code == 200:
+                break
+            print "Failed to fetch stock %s %d time(s)" % (symbol, ii + 1)            
         r.raise_for_status()
         lines = r.text.splitlines()
         json_data = json.loads(''.join([x for x in lines if x not in ('// [', ']')]))
